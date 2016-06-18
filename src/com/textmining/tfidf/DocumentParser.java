afc6330826306689d63b523379cfb37c7d1c7119
Bramantyo Adhilaksono <bramantyoadhilaksono@gmail.com>
2016-06-18 11:24:32 +0700

    Tf-Idf basic

---
+++ src/com/textmining/tfidf/DocumentParser.java
@@ -0,0 +1,101 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.textmining.tfidf;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class to read documents
+ *
+ * @author Mubin Shrestha
+ */
+public class DocumentParser {
+
+    //This variable will hold all terms of each document in an array.
+    private List<String[]> termsDocsArray = new ArrayList<String[]>();
+    private List<String> allTerms = new ArrayList<String>(); //to hold all terms
+    private List<double[]> tfidfDocsVector = new ArrayList<double[]>();
+
+    /**
+     * Method to read files and store in array.
+     * @param filePath : source file path
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public void parseFiles(String filePath) throws FileNotFoundException, IOException {
+        System.out.println("Initiating file parsing\nGetting available file(s)");
+        File[] allfiles = new File(filePath).listFiles();
+        System.out.println("Starting buffered reader");
+        BufferedReader in = null;
+        for (File f : allfiles) {
+            System.out.println("Starting file(s) loop");
+            if (f.getName().endsWith(".txt")) {
+                in = new BufferedReader(new FileReader(f));
+                StringBuilder sb = new StringBuilder();
+                String s = null;
+                while ((s = in.readLine()) != null) {
+                    sb.append(s);
+                }
+                String[] tokenizedTerms = sb.toString().replaceAll("[\\W&&[^\\s]]", "").split("\\W+");   //to get individual terms
+                for (String term : tokenizedTerms) {
+                    if (!allTerms.contains(term)) {  //avoid duplicate entry
+                        allTerms.add(term);
+                    }
+                }
+                termsDocsArray.add(tokenizedTerms);
+            }
+        }
+
+    }
+
+    /**
+     * Method to create termVector according to its tfidf score.
+     */
+    public void tfIdfCalculator() {
+        double tf; //term frequency
+        double idf; //inverse document frequency
+        double tfidf; //term requency inverse document frequency
+
+        System.out.println("Starting computing Tf-Idf");        
+        for (String[] docTermsArray : termsDocsArray) {
+            double[] tfidfvectors = new double[allTerms.size()];
+            int count = 0;
+            for (String terms : allTerms) {
+                System.out.println("Term: "+terms);
+                tf = new TfIdf().tfCalculator(docTermsArray, terms);
+                idf = new TfIdf().idfCalculator(termsDocsArray, terms);
+                tfidf = tf * idf;
+                System.out.println("Value: "+tfidf+"\n");
+                tfidfvectors[count] = tfidf;
+                count++;
+            }
+            tfidfDocsVector.add(tfidfvectors);  //storing document vectors;            
+        }
+    }
+
+    /**
+     * Method to calculate cosine similarity between all the documents.
+     */
+    public void getCosineSimilarity() {
+        System.out.println("Starting computing Cosine Similarity");
+        for (int i = 0; i < tfidfDocsVector.size(); i++) {
+            for (int j = 0; j < tfidfDocsVector.size(); j++) {
+                System.out.println("between " + i + " and " + j + "  =  "
+                                   + new CosineSimilarity().cosineSimilarity
+                                       (
+                                         tfidfDocsVector.get(i), 
+                                         tfidfDocsVector.get(j)
+                                       )
+                                  );
+            }
+        }
+    }
+}
---
+++ src/com/textmining/tfidf/TfIdf.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.textmining.tfidf;
+
+import java.util.List;
+
+//<editor-fold defaultstate="collapsed" desc="TFIDF calculator">
+/**
+ * Class to calculate TfIdf of term.
+ * @author Mubin Shrestha
+ */
+public class TfIdf {
+    
+    //<editor-fold defaultstate="collapsed" desc="TF Calculator">
+    /**
+     * Calculated the tf of term termToCheck
+     * @param totalterms : Array of all the words under processing document
+     * @param termToCheck : term of which tf is to be calculated.
+     * @return tf(term frequency) of term termToCheck
+     */
+    public double tfCalculator(String[] totalterms, String termToCheck) {
+        System.out.println("Computing Term-Frequency");
+        double count = 0;  //to count the overall occurrence of the term termToCheck
+        for (String s : totalterms) {
+            if (s.equalsIgnoreCase(termToCheck)) {
+                count++;
+            }
+        }
+        System.out.println("Done computing Term-Frequency");
+        return count / totalterms.length;
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Idf Calculator">
+    /**
+     * Calculated idf of term termToCheck
+     * @param allTerms : all the terms of all the documents
+     * @param termToCheck
+     * @return idf(inverse document frequency) score
+     */
+    public double idfCalculator(List<String[]> allTerms, String termToCheck) {
+        double count = 0;
+        System.out.println("Computing Inverse Document Frequency");
+        for (String[] ss : allTerms) {
+            for (String s : ss) {
+                if (s.equalsIgnoreCase(termToCheck)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        System.out.println("Done computing Inverse Document Frequency");
+        return Math.log(allTerms.size() / count);
+    }
+//</editor-fold>
+}
+//</editor-fold>
